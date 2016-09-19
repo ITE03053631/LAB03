def049e5425de653eb7d847c25c0a53d94d11960
john <john@john-VirtualBox.(none)>
2016-09-19 17:10:40 +0800

    9/19

---
+++ haha.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  
+ int main()
+ {
+  int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
+  printf("請輸入十個數字，用空格隔開"); 
+  scanf("%d %d %d %d %d %d %d %d %d %d",&n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10);
+  printf("您輸入的數字為 %d %d %d %d %d %d %d %d %d %d",           n1,n2,n3,n4,n5,n6,n7,n8,n9,n10);
+ }
